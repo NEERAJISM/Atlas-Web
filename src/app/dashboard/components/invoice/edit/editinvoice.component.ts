b87087a253653225248788883be5b08f41e71248
NEERAJISM <8patidarneeraj@gmail.com>
2020-09-09 01:04:32 +0530

    20200909 - invoice formatting

--- src/app/dashboard/components/invoice/edit/editinvoice.component.ts
+++ src/app/dashboard/components/invoice/edit/editinvoice.component.ts
@@ -29,13 +29,12 @@ export class EditInvoiceComponent {
   items: Item[] = [];
 
   title = 'jspdf-autotable-demo';
-  head = [['ID', 'Country', 'Rank', 'Capital']];
 
-  headOwnerAddress = [['Invoice# 1254-5621', 'From : Krishna Borewell']];
+  headOwnerAddress = [['Invoice # 1254-5621', 'Seller : Krishna Borewell']];
 
   dataOwnerAddress = [
-    ['Issue Date   : 20 Sep 2020', 'C\o Balkrishna Patidar, Gamda Brahmaniya'],
-    ['Due   Date   : 20 Sep 2020', 'Sagwara, Dungarpur (Raj) - 314025'],
+    ['Issue Date     : 20 Sep 2020', 'C\o Balkrishna Patidar, Gamda Brahmaniya'],
+    ['Due   Date     : 20 Sep 2020', 'Sagwara, Dungarpur (Raj) - 314025'],
     ['Supply Place : Sagwara', 'Email : 8patidarneeraj@gmail.com'],
     ['', 'Mob : +91 - 8877073059']
   ];
@@ -44,47 +43,21 @@ export class EditInvoiceComponent {
   headSellerAddress = [['Billing To : \nNeeraj Patidar', 'Shipping To : \nRajat Jain Akhawat']];
 
   dataSellerAddress = [
-    ['C\o Balkrishna Patidar, Gamda Brahmaniya', '27,Lakshmi Marg, Amal ka kanta, Surajpole Police St'],
+    ['C\o Balkrishna Patidar, Gamda Brahmaniya', '27,Lakshmi Marg, Amal ka kanta,'],
     ['Sagwara, Dungarpur (Raj) - 314025', 'Udaipur (Raj) - 313001'],
     ['Email : 8patidarneeraj@gmail.com', 'Email : rajatjainakhawat@gmail.com'],
     ['Mob : +91 - 8877073059', 'Mob : +91 - 9929307208']
   ];
 
+  head = [['No.', 'Item Description', 'Code', 'Qty', 'Price', 'Amount', 'SGST', 'CGST', 'Total']];
+
   data = [
-    [1, 'Finland', 7.632, 'Helsinki'],
-    [2, 'Norway', 7.594, 'Oslo'],
-    [3, 'Denmark', 7.555, 'Copenhagen'],
-    [4, 'Iceland', 7.495, 'Reykjavík'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [5, 'Switzerland', 7.487, 'Bern'],
-    [6, 'Sweden', 7.314, 'Stockholm'],
-    [7, 'Belarus', 5.483, 'Minsk'],
+    [1, 'Sulphuric Acid 500 ml bottle', 532, 5, 500.0, 2500.0, '225.0\n(9%)', '225.0\n(9%)', 2950.0],
+    [1, 'Sulphuric Acid 500 ml bottle', 532, 5, 500.0, 2500.0, '225.0\n(9%)', '225.0\n(9%)', 2950.0],
+    [1, 'Sulphuric Acid 500 ml bottle', 532, 5, 500.0, 2500.0, '225.0\n(9%)', '225.0\n(9%)', 2950.0],
+    [1, 'Sulphuric Acid 500 ml bottle', 532, 5, 500.0, 2500.0, '225.0\n(9%)', '225.0\n(9%)', 2950.0],
+    [1, 'Sulphuric Acid 500 ml bottle', 532, 5, 500.0, 2500.0, '225.0\n(9%)', '225.0\n(9%)', 2950.0],
   ];
-
-
-
 
   constructor(private router: Router) {
     this.loadClients();
@@ -93,7 +66,6 @@ export class EditInvoiceComponent {
   loadClients(){
 
   }
-
 
   goBackToInvoiceComponent() {
     this.router.navigateByUrl('/dashboard/invoice');
@@ -126,37 +98,44 @@ export class EditInvoiceComponent {
 
   public downloadPDF(): void {
 
-    // const DATA = this.htmlData.nativeElement;
     const doc = new jsPDF ();
 
     doc.addImage("../../assets/icons/atlas-small.png", 'PNG', 185, 7, 17, 17);
 
     doc.setFontSize(30);
     doc.text('TAX INVOICE', 13, 20);
-    
+
     (doc as any).autoTable({
-      startY: 40,
+      startY: 30,
       head: this.headOwnerAddress,
       body: this.dataOwnerAddress,
       theme: 'plain',
-      styles: {fontSize: '11', cellPadding: {top: 1, right: 1, bottom: 0, left: 1}}
+      headStyles: {fontSize: '13'},
+      styles: {cellWidth: 95, fontSize: '11', cellPadding: {top: 1, right: 1, bottom: 0, left: 1}}
     });
 
     (doc as any).autoTable({
-      startY: 80,
+      startY: 65,
       head: this.headSellerAddress,
       body: this.dataSellerAddress,
       theme: 'plain',
-      styles: {fontSize: '11', cellPadding: {top: 1, right: 1, bottom: 0, left: 1}}
+      headStyles: {fontSize: '13'},
+      styles: {cellWidth: 95, fontSize: '11', cellPadding: {top: 1, right: 1, bottom: 0, left: 1}}
     });
 
 
     (doc as any).autoTable({
-      startY: 120,
+      startY: 110,
       head: this.head,
       body: this.data,
       theme: 'striped'
     });
+
+    doc.setFontSize(12);
+    let finalY = (doc as any).lastAutoTable.finalY;
+
+    doc.line(50, finalY + 40, 100, finalY + 40);
+    doc.text('Authorized Signature', 50, finalY + 50);
 
     // fOOTER
     // doc.setFontSize(11);
@@ -165,18 +144,3 @@ export class EditInvoiceComponent {
     doc.save('atlas.pdf');
   }
 }
-
-
-
-
-  //   doc.html(DATA.innerHTML, {
-  //     callback: function (doc) {
-  //       doc.save('atlas.pdf');
-  //     }
-  //  });
-
-    // doc.text("Hello world!", 10, 10);
-
-    // doc.addHTML(DATA.innerHTML, function() {
-    //   console.log('pdf generated');
-    // });
